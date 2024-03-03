7baab7b9d5441b016473a9ea5deb5e3d4d599d1a
knowledgegainer1 <clonemaker74@gmail.com>
2024-03-03 19:08:18 +0530

    ade

--- provider.tf
+++ provider.tf
@@ -1,19 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.31.0" # AWS provider version, not terraform version
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
-
-  backend "s3" {
-    bucket = "daws76s-remote-state"
-    key    = "eksctl"
-    region = "us-east-1"
-    dynamodb_table = "daws76s-locking"
-  }
+  backend "s3"{
+  bucket = "tfstate-ssr-bucket"
+  key = "eksctl"
+  region = "us-east-1"
+  dynamodb_table = "robo-dev"
+}
 }
 
+# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
+
